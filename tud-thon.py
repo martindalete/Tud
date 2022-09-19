f4ad4088d8ab05b6159808878c510071a3424a27
Tyler Martindale <68250419+martindalete@users.noreply.github.com>
2022-09-19 14:02:00 -0400

    Update tud-thon.py

--- tud-thon.py
+++ tud-thon.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="Tud is")
 st.title("Tud is a ")
 
 nouns = pd.read_csv(r"https://raw.githubusercontent.com/martindalete/Tud/main/91K nouns.txt", header = None, names = ['Noun'])
-adverbs = pd.read_csv(r"https://raw.githubusercontent.com/martindalete/Tud/main/nouns\6K adverbs.txt", header = None, names = ['Adverb'])
+adverbs = pd.read_csv(r"https://raw.githubusercontent.com/martindalete/Tud/main/6K adverbs.txt", header = None, names = ['Adverb'])
 adjectives = pd.read_csv(r"https://raw.githubusercontent.com/martindalete/Tud/main/28K adjectives.txt", header = None, names = ['Adjective'])
 
 def display_button():
