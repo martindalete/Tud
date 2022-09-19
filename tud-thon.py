394d10c6fa0fcf3781db3b98e9f11e9a94659151
Tyler Martindale <68250419+martindalete@users.noreply.github.com>
2022-09-19 14:02:55 -0400

    Update tud-thon.py

--- tud-thon.py
+++ tud-thon.py
@@ -6,9 +6,9 @@ st.experimental_memo.clear()
 st.set_page_config(page_title="Tud is")
 st.title("Tud is a ")
 
-nouns = pd.read_csv(r"https://raw.githubusercontent.com/martindalete/Tud/main/91K nouns.txt", header = None, names = ['Noun'])
-adverbs = pd.read_csv(r"https://raw.githubusercontent.com/martindalete/Tud/main/6K adverbs.txt", header = None, names = ['Adverb'])
-adjectives = pd.read_csv(r"https://raw.githubusercontent.com/martindalete/Tud/main/28K adjectives.txt", header = None, names = ['Adjective'])
+nouns = pd.read_csv(r"https://raw.githubusercontent.com/martindalete/Tud/main/91Knouns.txt", header = None, names = ['Noun'])
+adverbs = pd.read_csv(r"https://raw.githubusercontent.com/martindalete/Tud/main/6Kadverbs.txt", header = None, names = ['Adverb'])
+adjectives = pd.read_csv(r"https://raw.githubusercontent.com/martindalete/Tud/main/28Kadjectives.txt", header = None, names = ['Adjective'])
 
 def display_button():
     st.button('What is Tud?')
